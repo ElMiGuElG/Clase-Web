7d38208960c99104bb6db3d56644d906a52c7e54
Miguel Ángel Galeano <113398998+ElMiGuElG@users.noreply.github.com>
2024-02-28 13:20:01 -0500

    Update clase3.js

--- javaScript/clase3.js
+++ javaScript/clase3.js
@@ -1,5 +1,5 @@
 //Declarar Una Variable sin inicial
-
+/*/
 let nombreUsuario;
 
 //Declarar Una Variable con inicial
@@ -66,7 +66,80 @@ function sumar (numeroUno,numeroDos){
     * Integrar con llamado a Funciones y Mostrar Resultado
 */
 
+//Clase #4
 
+//Arreglos (Arrays)
+let numeros = [10.34,1,1000,350.456]
+//Cualquier tipo de dato
+const varios = ["Juan",true,10,null,3.0]
 
+console.log(numeros);
+console.log(varios);
+console.log(varios[0]);
 
+console.log(varios.length);
 
+//Agregar Al Final 
+numeros.push("Colombia");
+console.log(numeros);
+
+//Quita el Ultimo
+numeros.pop();
+console.log(numeros);
+
+//Poner al Inicio 
+numeros.unshift("Brasil");
+console.log(numeros);
+
+//Quitar el Primero 
+numeros.shift();
+console.log(numeros);
+
+//Metodos para Arreglos 
+
+//1 .reduce ()
+
+let reducido = numeros.reduce((acumalador,valorActual) =>{
+    return acumalador+valorActual;
+});
+
+console.log(reducido);
+
+//2 .filter()
+
+let filtrado = numeros.filter(dato =>{
+    return dato >10;
+});
+
+console.log(filtrado);
+
+let strings = ["Colombia","Brasil","Comoros","Conecticut"]
+
+console.log(strings.filter(dato =>{
+    return dato.startsWith("Co");
+}));
+
+//3 .map()
+
+let mapeo = strings.map(pais =>{
+    return pais.concat("Hola");
+});
+
+console.log(mapeo);
+
+let mapped = numeros.map(numero =>{
+    return Math.pow(numero,2);
+});
+
+console.log(mapped);
+
+//4 .forEach()
+
+numeros.forEach(numero =>{
+    console.log(numero);
+});
+
+//Crear 4 arreglos usando funciones (Flecha)
+//la funcion debe crear el arreglo
+//Usar Metodos de arreglos y mostrar los resultados (para arraglo minimo un metodo)
+//Usar callbackfn
